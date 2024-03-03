4c59d368e86f303910743b9bddc7eea0bda38c16
AndrasGitServer <56222196+AndrasGitServer@users.noreply.github.com>
2024-03-03 12:27:17 -0500

    Add files via upload

---
+++ 01b-Variables-DATA-TYPES.py
@@ -0,0 +1,36 @@
+# Python Variables TYPES = DATA TYPES
+
+# All variable names (namespaces) are meaningful (descriptive).
+
+# 1. int    ->  whole numbers   ->  3, 30, 200
+count_integer = 100
+print(count_integer, type(count_integer) )
+
+# 2. float  ->  numbers with a decimal point    ->  2.3, 4.6, 100.0
+price = 4.99
+
+# 3. str    ->  ordered sequence of characters  ->  "Hello", "Concepts", "2000"
+computer_name = "Python Programming"
+
+# 4. bool   ->  logical value indicating True or False
+is_ready = True
+extra_boolean = False
+
+# 5. Collections (Arrays). There are 4 collection DATA TYPES in Python.
+#    ###########
+#   list    ->  ordered sequence of objects     ->  [10, "hello", 200.3, True, False]
+#   lists are: ordered, changeable, allows duplicates variables, uses BRACKETS []
+my_list =  [ 10, "hello", 200.3, True, False, True ]
+print(my_list)
+
+# 6. dictionary ->  ordered, changeable, indexed, no-duplicates.
+my_dict = { 'name':"parrot", 'color':"blue", 'qty':5 }
+print(my_dict)
+
+# 7. tupple ->  ordered immutable sequence of objects   ->  (10, "hello", 200.3)
+
+# 8. set    ->  unordered collection of unique objects  ->  {"a", "b", "c"}
+
+
+
+
